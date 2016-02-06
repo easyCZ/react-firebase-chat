89405030e40028b7ca94fc725dce1a5ffa1b10d4
Milan Pavlik <pavlik.mil@gmail.com>
2016-02-06 15:18:31 +0000

    Remove flexboxgrid ref

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
 import injectTapEventPLugin from 'react-tap-event-plugin';
-import flexboxgrid from 'flexboxgrid/dist/flexboxgrid.css';
 
 
 injectTapEventPLugin();
